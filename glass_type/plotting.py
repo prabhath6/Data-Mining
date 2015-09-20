6357535a28ea6ccadd53ca7e1c175127779f26b6
prabhath <prabhathkiran20@gmail.com>
2015-09-20 12:39:25 -0700

    Add parallel coordinate plot

--- glass_type/plotting.py
+++ glass_type/plotting.py
@@ -32,5 +32,34 @@ def boxplot(df):
     plt.ylabel("Quartile Ranges - Normalized ")
     plt.show()
 
+
+def coordinateplot(df):
+    """
+    :param df:
+    :return: parallel coordinate plot.
+    """
+
+    df = df.iloc[:, 1:len(df.columns)]
+    summary = df.describe()
+    normalizedvalues = df
+
+    ncols = len(df.columns)
+    nrows = len(df.index)
+
+    for i in range(ncols):
+        mean = summary.iloc[1, i]
+        sd = summary.iloc[2, i]
+
+        normalizedvalues.iloc[:, i:(i+1)] = (normalizedvalues.iloc[:, i:(i+1)] - mean) / sd
+
+    for i in range(nrows):
+        data_df = normalizedvalues.iloc[i, 1 : (ncols - 1)]
+        colorlabel = normalizedvalues.iloc[i, (ncols - 1)]/7.0
+        data_df.plot(color=plt.cm.RdYlBu(colorlabel), alpha=0.5)
+    plt.show()
+    plt.title("Parallel coordinate plot")
+    plt.savefig("Parallel coordinate plot")
+
 if __name__ == "__main__":
-    boxplot(df)
+    #boxplot(df)
+    coordinateplot(df)
